536c4088ce36a702b6785f441197968bd4926e94
yechanpark <qkrdpcks0419@naver.com>
2023-04-09 01:24:15 +0900

    initial commit

---
+++ src/main/kotlin/com/example/SpringBatchCustomize/SpringBatchCustomizeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SpringBatchCustomize
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchCustomizeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBatchCustomizeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SpringBatchCustomize/SpringBatchCustomizeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SpringBatchCustomize
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchCustomizeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
